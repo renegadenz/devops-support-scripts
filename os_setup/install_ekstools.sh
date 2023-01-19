ed6e4e9bc5b069b20e16476543ba66550ff04e53
EC2 Default User <ec2-user@ip-10-17-32-142.ap-southeast-2.compute.internal>
2023-01-19 20:11:48 +0000

    upload intial configuraiton

---
+++ os_setup/install_ekstools.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+package="zip"
+
+if yum list installed "$package" >/dev/null 2>&1; then
+  echo "$package is already installed"
+else
+  echo "Installing $package"
+  yum install -y "$package"
+fi
+
+#Install eksctl
+
+echo "Install eksctl"
+curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
+
+sudo mv /tmp/eksctl /usr/local/bin
+
+#Install eksctl
+echo "Install kubectl"
+curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.24.7/2022-10-31/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+mkdir -p $HOME/bin && cp ./kubectl $HOME/bin/kubectl && export PATH=$PATH:$HOME/bin
+echo 'export PATH=$PATH:$HOME/bin' >> ~/.bashrc
+
+#Install awscli
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+
